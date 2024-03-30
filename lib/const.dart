7622971e5c2b77c9b87f57f16681df4e9da935fa
thong-dipro <thongtd@dipro.vn>
2024-03-30 09:29:20 +0700

    fix: update constant to matching with native

--- lib/const.dart
+++ lib/const.dart
@@ -2,7 +2,7 @@ class AppConstant {
   static const String channelName = "demo-methodchanel";
 
   // Method name
-  static const String addTwoSum = "addTwoSum";
+  static const String addTwoSum = "twoSumNumber";
   static const String firstNum = "firstNum";
   static const String secondNum = "secondNum";
 }
